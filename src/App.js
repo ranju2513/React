8c85c27020b48590b10ac6b6e1ce8fbe15886dd8
ranju <ranjithaks136@gmail.com>
2022-08-17 12:47:24 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 /*import Message from './Message/Message'*/
 /*import Counter from './Counter/Counter'*/
-import Employee from './Employee/Employee'
+/*import Employee from './Employee/Employee'*/
+import Navbar from './Navbar/Navbar'
 class App extends React.Component {
     render() {
         return <div>
             <h1>App Component</h1>
             <hr />
             
-            <Employee />
+            <Navbar />
         </div>
     }
 }
